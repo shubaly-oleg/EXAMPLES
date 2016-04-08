592ebfdf7ba537c6cfc74711dfc449e49c01341a
Shubaly Oleg <shubaly.oleg@gmail.com>
2016-04-08 07:05:44 +0300

    new repository 4 examples and tests

---
+++ printArrguments/src/main/java/examples/printArrguments/PrintArrguments.java
@@ -0,0 +1,12 @@
+package examples.printArrguments;
+
+public class PrintArrguments 
+{
+    public static void main( String[] args )
+    {
+        int i = 0; 
+    	for (String str : args) {
+        	System.out.printf("Аргумент %02d-> %s%n", i++, str);
+		}
+    }
+}
