be1174179907d347b6079c69b973ecba149c41db
JAPmiratech <japmiratech@gmail.com>
2016-04-25 19:24:25 +0300

    ArrayList sample

---
+++ DemoGeneric/src/main/java/examples/DemoGeneric/DemoGeneric.java
@@ -0,0 +1,18 @@
+package examples.DemoGeneric;
+
+import java.util.ArrayList;
+
+public class DemoGeneric {
+
+	public static void main(String args[]) {
+		ArrayList<String> list = new ArrayList<String>();
+		// ArrayList<int> b = new ArrayList<int>(); // ошибка компиляции
+		list.add("Java"); //компилятор "знает" допустимый тип передаваемого значения
+		list.add("JavaFX 2");
+		String res = list.get(0); //компилятор "знает"допустимый тип возвращаемого значения
+		System.out.println(res); // удобный вывод
+		// list.add(new StringBuilder("C#")); // ошибка компиляции - компилятор не позволит 
+		// добавить "посторонний" тип
+		System.out.println(list); // удобный вывод
+	}
+}
