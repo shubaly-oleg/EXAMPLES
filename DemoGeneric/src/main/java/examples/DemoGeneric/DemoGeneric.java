cf893927b224d507ebd42c2e80bd51842318a3d1
Shubalyi Oleg <shubaly.oleg@gmail.com>
2016-04-26 01:55:18 +0300

    comment updated

--- DemoGeneric/src/main/java/examples/DemoGeneric/DemoGeneric.java
+++ DemoGeneric/src/main/java/examples/DemoGeneric/DemoGeneric.java
@@ -6,13 +6,14 @@ public class DemoGeneric {
 
 	public static void main(String args[]) {
 		ArrayList<String> list = new ArrayList<String>();
-		// ArrayList<int> b = new ArrayList<int>(); // ошибка компиляции
-		list.add("Java"); //компилятор "знает" допустимый тип передаваемого значения
+		// ArrayList<int> b = new ArrayList<int>(); // ошибка компиляции - примитивный
+		// тип недопустим - нужен объект (Integer)
 		list.add("JavaFX 2");
-		String res = list.get(0); //компилятор "знает"допустимый тип возвращаемого значения
-		System.out.println(res); // вывод перевого (индекс - 0) элемента коллекции
+		list.add("Java"); //компилятор "знает" допустимый тип передаваемого значения (String)
 		// list.add(new StringBuilder("C#")); // ошибка компиляции - компилятор не позволит 
-		// добавить "посторонний" тип
-		System.out.println(list); // вся коллекция
+		// добавить "посторонний" тип (StringBuilder) 
+		String res = list.get(0); //извлекаем первый (индекс - 0) элемент коллекции
+		System.out.println(res); // вывод перевого элемента коллекции
+		System.out.println(list); // вывод всей коллекции
 	}
 }
