de2e58f50be5b59d2d3c1b7eaa448d2bd78dc11c
Shubalyi Oleg <shubaly.oleg@gmail.com>
2016-05-06 21:27:44 +0300

    move time samples to TIME folder

---
+++ TIME/time.TimeDiff/src/main/java/examples/time/TimeDiff/TimeDiff.java
@@ -0,0 +1,13 @@
+package examples.time.TimeDiff;
+
+/**
+ * Hello world!
+ *
+ */
+public class TimeDiff 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
