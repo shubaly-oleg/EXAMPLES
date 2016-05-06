8838f0efeeb8fddad38aca535e9a13dd2f78a0a8
Shubalyi Oleg <shubaly.oleg@gmail.com>
2016-05-06 23:49:10 +0300

    ns parsed to ds:hr:s:ms:mks:ns

--- TIME/time.TimeDiff/src/main/java/examples/time/TimeDiff/TimeDiff.java
+++ TIME/time.TimeDiff/src/main/java/examples/time/TimeDiff/TimeDiff.java
@@ -1,13 +1,45 @@
 package examples.time.TimeDiff;
 
-/**
- * Hello world!
- *
- */
+import java.util.concurrent.TimeUnit;
+
 public class TimeDiff 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//    	long t = System.nanoTime();
+    	long t = 9999999523896752L;
+    	System.out.println( t );
+//    	System.out.println(TimeUnit.NANOSECONDS.toSeconds(t));
+
+    	int ns = (int) (t % 1000);
+    	System.out.println(ns + " ns");
+    	
+    	int mks = (int) ((t % 1000000 - ns) / 1000);
+    	System.out.println(mks + " mks");
+    	
+    	int ms = (int) ((t % 1000000000 - (mks*1000 + ns)) / 1000000);
+    	System.out.println(ms + " ms");
+    	
+    	long allSec = t / 1000000000 ;
+//    	System.out.println("allSec = " + allSec);
+    	
+    	int s = (int) (allSec % 60);
+    	System.out.println(s + " s");
+    	
+    	int allMin = (int) (allSec / 60);
+//    	System.out.println("allMin = " + allMin);
+    	
+    	int min = (int) (allMin % 60);
+    	System.out.println(min + " min");
+    	
+    	int allHr = (int) (allMin / 60);
+//    	System.out.println("allHr = " + allHr);
+    	
+    	int hr = (int) (allHr % 24);
+    	System.out.println(hr + " hr");
+    	
+    	int ds = (int) (allHr / 24);
+    	System.out.println(ds + " ds");
+    	
     }
 }
